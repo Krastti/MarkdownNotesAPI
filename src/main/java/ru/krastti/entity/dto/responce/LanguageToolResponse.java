091cdad53384ae2d6d40289b898e79d8934017a7
Krastti <salcev77@gmail.com>
2026-08-24 22:16:01 +0200

    update: LanguageToolResponse

--- src/main/java/ru/krastti/entity/dto/responce/LanguageToolResponse.java
+++ src/main/java/ru/krastti/entity/dto/responce/LanguageToolResponse.java
@@ -21,154 +21,154 @@ public class LanguageToolResponse {
     public void setMatches(List<Match> matches) {
         this.matches = matches;
     }
-}
 
-class Language {
-    private String name;
-    private String code;
+    public static class Language {
+        private String name;
+        private String code;
 
-    public String getName() {
-        return name;
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public static class Match {
+        private String message;
+        private String shortMessage;
+        private int offset;
+        private int length;
+        private List<Replacement> replacements;
+        private Rule rule;
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public String getShortMessage() {
+            return shortMessage;
+        }
+
+        public void setShortMessage(String shortMessage) {
+            this.shortMessage = shortMessage;
+        }
+
+        public int getOffset() {
+            return offset;
+        }
+
+        public void setOffset(int offset) {
+            this.offset = offset;
+        }
+
+        public int getLength() {
+            return length;
+        }
+
+        public void setLength(int length) {
+            this.length = length;
+        }
+
+        public List<Replacement> getReplacements() {
+            return replacements;
+        }
+
+        public void setReplacements(List<Replacement> replacements) {
+            this.replacements = replacements;
+        }
+
+        public Rule getRule() {
+            return rule;
+        }
+
+        public void setRule(Rule rule) {
+            this.rule = rule;
+        }
     }
 
-    public String getCode() {
-        return code;
+    public static class Replacement {
+        private String value;
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-}
+    public static class Rule {
+        private String id;
+        private String description;
+        private String issueType;
+        private Category category;
 
-class Match {
-    private String message;
-    private String shortMessage;
-    private int offset;
-    private int length;
-    private List<Replacement> replacements;
-    private Rule rule;
+        public String getId() {
+            return id;
+        }
 
-    public String getMessage() {
-        return message;
-    }
+        public void setId(String id) {
+            this.id = id;
+        }
 
-    public void setMessage(String message) {
-        this.message = message;
-    }
+        public String getDescription() {
+            return description;
+        }
 
-    public String getShortMessage() {
-        return shortMessage;
-    }
+        public void setDescription(String description) {
+            this.description = description;
+        }
 
-    public void setShortMessage(String shortMessage) {
-        this.shortMessage = shortMessage;
-    }
+        public String getIssueType() {
+            return issueType;
+        }
 
-    public int getOffset() {
-        return offset;
-    }
+        public void setIssueType(String issueType) {
+            this.issueType = issueType;
+        }
 
-    public void setOffset(int offset) {
-        this.offset = offset;
-    }
+        public Category getCategory() {
+            return category;
+        }
 
-    public int getLength() {
-        return length;
+        public void setCategory(Category category) {
+            this.category = category;
+        }
     }
 
-    public void setLength(int length) {
-        this.length = length;
-    }
+    public static class Category {
+        private String id;
+        private String name;
 
-    public List<Replacement> getReplacements() {
-        return replacements;
-    }
+        public String getId() {
+            return id;
+        }
 
-    public void setReplacements(List<Replacement> replacements) {
-        this.replacements = replacements;
-    }
+        public void setId(String id) {
+            this.id = id;
+        }
 
-    public Rule getRule() {
-        return rule;
-    }
+        public String getName() {
+            return name;
+        }
 
-    public void setRule(Rule rule) {
-        this.rule = rule;
-    }
-}
-
-class Replacement {
-    private String value;
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-}
-
-class Rule {
-    private String id;
-    private String description;
-    private String issueType;
-    private Category category;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getIssueType() {
-        return issueType;
-    }
-
-    public void setIssueType(String issueType) {
-        this.issueType = issueType;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-}
-
-class Category {
-    private String id;
-    private String name;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
