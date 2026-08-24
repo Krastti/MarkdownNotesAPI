f742aecd40aebe862e3ca105751984c2532bf880
Krastti <salcev77@gmail.com>
2026-08-25 00:59:12 +0200

    Перенес всё на мульти модальную систему

--- notes-api/src/main/java/ru/krastti/NotesApiApplication.java
+++ notes-api/src/main/java/ru/krastti/NotesApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotesRestServiceApplication {
+public class NotesApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotesRestServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotesApiApplication.class, args);
+    }
 
 }
