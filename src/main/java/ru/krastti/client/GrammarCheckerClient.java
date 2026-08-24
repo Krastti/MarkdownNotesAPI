8a060ae16743deecdbd82c950dbb035582e0a765
Krastti <salcev77@gmail.com>
2026-08-24 21:44:35 +0200

    update: GrammarCheckerClient.java

--- src/main/java/ru/krastti/client/GrammarCheckerClient.java
+++ src/main/java/ru/krastti/client/GrammarCheckerClient.java
@@ -3,9 +3,10 @@ package ru.krastti.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClient;
 import ru.krastti.entity.dto.NoteDTO;
-import ru.krastti.entity.dto.request.LanguageToolRequest;
 import ru.krastti.entity.dto.responce.LanguageToolResponse;
 
 @Component
@@ -17,11 +18,15 @@ public class GrammarCheckerClient {
         this.restClient = restClient;
     }
 
-    public LanguageToolResponse grammarCheck(NoteDTO body) {
+    public LanguageToolResponse grammarCheck(NoteDTO note) {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>(); // TODO Добавить сериализацию DTO класса
+        formData.add("text", note.text());
+        formData.add("language", "ru-RU"); // TODO Сделать настраиваемым
+
         return restClient.post()
                 .uri("/v2/check")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(LanguageToolRequest.class)
+                .body(formData)
                 .retrieve()
                 .body(LanguageToolResponse.class);
     }
